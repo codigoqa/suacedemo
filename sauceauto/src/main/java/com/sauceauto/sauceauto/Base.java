54c7fee09f83084fdeedf584b7feecb45fef887f
Virgil <102872659+devvirgil@users.noreply.github.com>
2023-06-08 15:52:17 -0400

    'first'

---
+++ sauceauto/src/main/java/com/sauceauto/sauceauto/Base.java
@@ -0,0 +1,15 @@
+package com.sauceauto.sauceauto;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Base {
+  public static void main(String[] args) {
+    
+    WebDriverManager.chromedriver().setup();
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://www.saucedemo.com/");
+    
+  }
+}
---
+++ sauceauto/src/test/java/com/sauceauto/sauceauto/AppTest.java
@@ -0,0 +1,13 @@
+package com.sauceauto.sauceauto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
